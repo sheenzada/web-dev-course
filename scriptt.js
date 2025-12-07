3e298a2b4dfe368b62c4eef9a1c2bd846f64f5f8
Inamullah <sheenzada7@gmail.com>
2025-12-07 04:43:59 -0800

    js

---
+++ scriptt.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const items = document.querySelectorAll('.animate-item');
+    items.forEach((item,index)=>{
+        setTimeout(() =>{
+            item.classList.add('show');
+        },index*900);
+    })
+})
