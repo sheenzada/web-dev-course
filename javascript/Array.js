d4df88b0ae032f5b1fbd2d4af1db8b020e6cf5bd
Inamullah <sheenzada7@gmail.com>
2025-12-15 03:43:45 -0800

    log

---
+++ javascript/Array.js
@@ -0,0 +1,4 @@
+let arr=[1,3,4,5,6]
+console.log(arr);
+
+console.log(arr[5]);
--- javascript/question.js
+++ javascript/question.js
@@ -1,4 +1,4 @@
-// // Question No 1
+// Question No 1
 // let a = 1;
 //     console.log("do");
 // do {
@@ -7,9 +7,35 @@
     
 // } while(a<=50)
     
-// Question No 2
-let v = 4;
-for( v > 1 ;  v-- ; ){
-    console.log(v);
+// // Question No 2
+// let v = 4;
+// for( v > 1 ;  v-- ; ){
+//     console.log(v);
+    
+// }
+
+// for(let i = 1; i<50 ; i++){
+//     if(i%3===0 && i%5===0){
+//         console.log("divide");
+        
+//     }
+// }
+
+// let num1=0, num2=1, N=8;
+// for (ler i=1; i<=N; i++){
+
+// }
+let a =10;
+for(let i = 0; i<10; i++){
+    row="";
+    if(i%2==0){
+        r=""
+        console.log("*");
+        
+    }
+    console.log(r);
     
 }
+
+let ab=10;
+for(i=0; i<10;)
