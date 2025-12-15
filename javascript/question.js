fccee4f637b48c512e8c871d39d5eae14c18afb7
Inamullah <sheenzada7@gmail.com>
2025-12-15 02:20:04 -0800

    home work

---
+++ javascript/question.js
@@ -0,0 +1,15 @@
+// // Question No 1
+// let a = 1;
+//     console.log("do");
+// do {
+//     console.log(a);
+//     a++;
+    
+// } while(a<=50)
+    
+// Question No 2
+let v = 4;
+for( v > 1 ;  v-- ; ){
+    console.log(v);
+    
+}
