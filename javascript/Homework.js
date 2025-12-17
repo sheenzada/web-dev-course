eabc7723d22e17f8d577f88bb5218ccfef174b2a
Inamullah <sheenzada7@gmail.com>
2025-12-17 01:49:52 -0800

    home work

---
+++ javascript/Homework.js
@@ -0,0 +1,36 @@
+let arr=["inam ullah","najmul akash","yaseen malik","tajwar shaheen"]
+console.log(arr);
+
+console.log(arr[2]);
+
+arr.pop();
+arr.pop();
+arr.pop();
+arr.pop();
+arr.pop();
+
+arr.push("ali");
+console.log(arr);
+console.log(arr[0]);
+
+let [a,b,c ,...res]=arr;
+console.log(c);
+console.log(res);
+console.log(b);
+console.log(res);
+console.log(a);
+console.log(res);
+
+
+let C = [1,2,3,4,[5,6,7],7,8,9];
+console.log(C);
+
+let B = ["a","b",["c","d"],"e","f"];
+console.log(B);
+
+let A = ["!","@",["#","$"],"%","&"];
+console.log(A);
+
+
+
+
