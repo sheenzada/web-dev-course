edf8be1576fa554761a69e7a7b66e48c4395273e
Inamullah <sheenzada7@gmail.com>
2025-12-17 03:39:02 -0800

    object

---
+++ javascript/qu.js
@@ -0,0 +1,38 @@
+// const arr = [2,4,6];
+// const [first , ...rest]=arr;
+// const last = rest [rest.length - 1];
+
+// const sum = first + last;
+// console.log(sum % 2 ===0 ? "Even": "Odd");
+// if(sum%2===0){
+//     console.log("even");
+    
+// }
+
+// // task 2
+
+// const arr1 = [34,6];
+// let max = arrarr[0];
+
+// for(let i = 1 ; i < arr1.length; i++){
+//     if(arr1[i] > max){
+//         max =arr1[i];
+//     }
+// }
+// console.log(max );
+//
+// object
+let person ={
+    name:"inam",
+    PhNo: "03498405661",
+    id:4545,
+    address:{
+        city:"Islamabad",
+        zip:16100,
+        street:"Hargisa 106",
+
+    }
+}
+// console.log(person.id);
+console.log(person.address);
+
