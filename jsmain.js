72c95fdde16f78477565faa77b5b24cffada9634
Inamullah <sheenzada7@gmail.com>
2025-12-24 02:39:25 -0800

    function

---
+++ jsmain.js
@@ -0,0 +1,8 @@
+function showAlert() {
+  alert("Welcome to the website!");
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  alert("Message sent successfully!");
+}
---
+++ scripttt.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+  const nav = document.getElementById("nav");
+  nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+}
