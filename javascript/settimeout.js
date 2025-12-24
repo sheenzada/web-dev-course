d1ecd2b892af6d4ede8b3d33c2fcb1da39abe68e
Inamullah <sheenzada7@gmail.com>
2025-12-24 02:04:15 -0800

    function

--- javascript/settimeout.js
+++ javascript/settimeout.js
@@ -10,36 +10,32 @@ function sums() {
 }
 sums()
 
-// setTimeout(() => {
-//         console.log("function run");
-        
-// }, 3000);
-// setTimeout();
-
-// let inam=setTimeout(() => {
-//     console.log("The function is running");
-    
-// },2000);
-// inam()
-
-// let person={
-//     firstName:"inam",
-//     lastName:"Ullah",
-//     FullName:()=>{
-//         console.log(`${this.firstName} ${this.lastName}`);
-        
-//     }
-// }
-// console.log(person.FullName);
-// The rest of your script continues to run immediately.
-console.log("Start of script.");
-
 setTimeout(() => {
-  console.log("This message appears after 2 seconds (2000ms)."); // This runs later
-}, 2000);
+        console.log("function run");
+        
+}, 3000);
+setTimeout();
 
-console.log("End of script.");
-// Output in console:
-// Start of script.
-// End of script.
-// This message appears after 2 seconds (2000ms).
+let inam=setTimeout(() => {
+    console.log("The function is running");
+    
+},2000);
+inam()
+
+let person={
+    firstName:"inam",
+    lastName:"Ullah",
+    FullName:()=>{
+        console.log(`${this.firstName} ${this.lastName}`);
+        
+    }
+}
+console.log(person.FullName);
+// console.log("Start of script.");
+
+// setTimeout(() => {
+//   console.log("This message appears after 2 seconds (2000ms)."); // This runs later
+// }, 2000);
+
+// console.log("End of script.");
+
