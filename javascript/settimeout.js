3c42baf01021231569bc7f7906348b95b4065990
Inamullah <sheenzada7@gmail.com>
2025-12-24 02:10:25 -0800

    function

--- javascript/settimeout.js
+++ javascript/settimeout.js
@@ -10,32 +10,35 @@ function sums() {
 }
 sums()
 
-setTimeout(() => {
-        console.log("function run");
+//  setTimeout(() => {
+//         console.log("function run");
         
-}, 3000);
-setTimeout();
+// }, 3000);
+// setTimeout();
 
-let inam=setTimeout(() => {
-    console.log("The function is running");
+
+// let inam=setTimeout(() => {
+//     console.log("The function is running");
     
-},2000);
-inam()
+// },2000);
+// inam()
 
-let person={
-    firstName:"inam",
-    lastName:"Ullah",
-    FullName:()=>{
-        console.log(`${this.firstName} ${this.lastName}`);
+// let person={
+//     firstName:"inam",
+//     lastName:"Ullah",
+//     FullName:()=>{
+//         console.log(`${this.firstName} ${this.lastName}`);
         
-    }
-}
-console.log(person.FullName);
-// console.log("Start of script.");
+//     }
+// }
+// console.log(person.FullName);
 
-// setTimeout(() => {
-//   console.log("This message appears after 2 seconds (2000ms)."); // This runs later
-// }, 2000);
 
-// console.log("End of script.");
+console.log("Start of script.");
+
+setTimeout(() => {
+  console.log("This message appears after 2 seconds (2000ms)."); // This runs later
+}, 5000);
+
+console.log("End of script.");
 
