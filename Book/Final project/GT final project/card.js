f92f8705af4955f1830638fb93c8bdda58fbc173
Inamullah <sheenzada7@gmail.com>
2026-01-08 02:28:15 -0800

    function

---
+++ Book/Final project/GT final project/card.js
@@ -0,0 +1,66 @@
+
+// login form
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("myBtn")[0];
+var btn = document.getElementsByClassName("myBtn")[1];
+var btn = document.getElementsByClassName("myBtn")[2];
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+function fun(){
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
---
+++ Book/Final project/Website of Acadmey/js/drack.js
@@ -0,0 +1,13 @@
+function dark(darkChek){
+   
+    if(darkChek.value==2)
+    {
+      document.body.style.backgroundColor='black', document.body.style.color='white';
+     
+    }
+    
+   else if(darkChek.value==1){
+      document.body.style.backgroundColor='white' , document.body.style.color='black';
+
+    }
+  }
