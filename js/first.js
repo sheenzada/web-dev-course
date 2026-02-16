71513ca5bfda0508453218c0d5086de41147d796
Inamullah <sheenzada7@gmail.com>
2026-02-16 04:41:22 -0800

    py

---
+++ js/first.js
@@ -0,0 +1,48 @@
+// Name = 'Inam'
+// age = 18
+// price = 99.99
+// radius = 12
+// x = null
+// y = undefined
+// isFollow = true;
+
+// Name = 25
+
+// console.log(Name);
+
+
+// let Names = 'inam'
+// let ages = 45
+// var agess = 56
+// console.log(agess);
+// console.log(ages);
+// console.log(Names);
+// console.log(age);
+
+// #_________----#________
+
+{
+    let a = 5;
+    console.log(a);
+    
+}
+{
+    let a = 10;
+    console.log(a);
+    
+}
+
+// const student= {
+//     Name : 'InamUllah',
+//     age : 18,
+//     school : 'PS&CS',
+// };
+// student['age']  = student['age'] + 1;
+// console.log(student["age"]);
+
+// const product = {
+//     Pen : 'Ball Pen',
+//     Price: 30,
+//     Quality: 1
+// }
+// console.log(typeof product);
