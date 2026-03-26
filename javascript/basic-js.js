fe158a3b1f8904ec6467404db5aee3dfcc8b15b2
Inamullah <sheenzada7@gmail.com>
2026-03-26 03:46:27 -0700

    python

---
+++ javascript/basic-js.js
@@ -0,0 +1,8 @@
+// Variable declare karna
+let name = "Ali";          // String
+let age = 25;              // Number
+const pi = 3.14;           // Constant (jiski value change nahi ho sakti)
+let isStudent = true;      // Boolean (True/False)
+
+console.log("Name:", name);
+console.log("Age:", age);
